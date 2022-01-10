46e211c66376ca853dda43faead9f7629472a205
JoseNguyen <abc@example.com>
2022-01-10 18:24:00 +0700

    Initial commit

---
+++ app/src/main/java/com/example/rxjavarxandroid/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.rxjavarxandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        ArrayList<String> arr = new ArrayList<>();
+
+        arr.add("Tèo");
+        arr.add("Tí");
+        arr.add("Tủn");
+
+
+        Iterator<String> data = arr.iterator();
+        while(data.hasNext())    {
+            Log.d("BBB",data.next());
+        }
+
+    }
+}

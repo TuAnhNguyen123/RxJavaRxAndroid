ff54afa07e1aaae3a2639df8390edde495f2be5e
JoseNguyen <abc@example.com>
2022-01-12 17:54:39 +0700

    completableFutere

--- app/src/main/java/com/example/rxjavarxandroid/MainActivity.java
+++ app/src/main/java/com/example/rxjavarxandroid/MainActivity.java
@@ -1,32 +1,79 @@
 package com.example.rxjavarxandroid;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class MainActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        ArrayList<String> arr = new ArrayList<>();
-
-        arr.add("Tèo");
-        arr.add("Tí");
-        arr.add("Tủn");
-
-
-        Iterator<String> data = arr.iterator();
-        while(data.hasNext())    {
-            Log.d("BBB",data.next());
+        try {
+            int total = sum(1,2).get();
+            int result = minus(total,3).get();
+            Log.d("BBB","Total : " + total);
+            Log.d("BBB","Result : " + result);
+            Log.d("BBB","Done");
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
 
     }
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private CompletableFuture<Integer> sum(int a, int b)    {
+
+        ExecutorService executorService = Executors.newFixedThreadPool(1);
+
+        CompletableFuture<Integer> completableFuture = new CompletableFuture<>();
+
+        executorService.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(2000);
+                    int c = a +b ;
+                    completableFuture.complete(c);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        return completableFuture;
+    }
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private CompletableFuture<Integer> minus(int a, int b)    {
+
+        ExecutorService executorService = Executors.newFixedThreadPool(1);
+
+        CompletableFuture<Integer> completableFuture = new CompletableFuture<>();
+        executorService.execute(new Runnable() {
+            @Override
+            public void run() {
+                int c = a-b ;
+                completableFuture.complete(c);
+
+            }
+        });
+        return completableFuture ;
+    }
+
 }
